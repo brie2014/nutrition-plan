2d8432211ea420751fbabc2a047e8426842aa7b6
Brienna Sambrano <brie2014@byui.edu>
2022-03-02 19:15:04 -0700

    test change

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
 app.use('/food', foodRoutes)
 
 //mongodb connection example 
+// adding a comment for a test
 // mongoose
 //     .connect('mongodb+srv://brie2014:Ilovebackend@cluster0.gn4ni.mongodb.net/social?retryWrites=true&w=majority')
 //     .then(app.listen(8080))
