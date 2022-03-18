dbecec82602c742cb5319310c7e09d3945eb5272
Adrian Whetten <46587336+Adrian-Whetten@users.noreply.github.com>
2022-03-18 17:34:22 -0600

    Completed the Find one food item and post food item

--- routes/food.js
+++ routes/food.js
@@ -7,6 +7,9 @@ const router = express.Router()
 // GET /foods
 router.get('/', foodController.getAllFoods)
 
+// //GET Single food item by ID
+// router.get('/:id', foodController.getFoodItem)
+
 // POST /food
 router.post('/add-food', foodController.createFood)
 
