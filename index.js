fddebabe32185daa6a271d2ec7ed178759679f50
couchpotatodev <mjrimayaho@gmail.com>
2022-03-26 22:10:49 +0800

    created env file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-
+const PORT = process.env.PORT || 8080
 const authRoutes = require('./routes/auth')
 const foodRoutes = require('./routes/food')
 
@@ -16,12 +17,37 @@ app.use((req, res, next) => {
     next()
 })
 
+const cors = require('cors') 
+const corsOptions = {
+    origin: "https://nutrition-plan2022.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
 app.use('/auth', authRoutes)
 app.use('/food', foodRoutes)
+app.use('/', (req,res)=>{
+    res.send('Welcome to my Nutritional plan')
+})
 
 //mongodb connection example 
 // adding a comment for a test
 mongoose
-    .connect('mongodb+srv://healthNut32:workingHard@cluster0.7opmt.mongodb.net/DietData')
-    .then(app.listen(8080))
+    //.connect('mongodb+srv://healthNut32:workingHard@cluster0.7opmt.mongodb.net/DietData')
+    .connect(process.env.MONGO_URL)
+    .then(app.listen(PORT))
+    .then(console.log('connected to database'))
+    //.env files contains the following:
+    //MONGO_URL=mongodb+srv://healthNut32:workingHard@cluster0.7opmt.mongodb.net/DietData
+    //PORT=8080
+    //secretKey=somesupersecretsecret
+    
     .catch(err => console.log(err))
